d7915743eec837df40d75c184544911b3ace7e38
TomKim <babooteng2@gmail.com>
2022-01-04 01:40:46 +0900

    #2.1 Fetching Data

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const API_KEY = process.env.API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [{
+      source: "/api/movies",
+      destination: `${BASE_URL}/movie/popular?api_key=${API_KEY}`
+    }]
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
-import Seo from "../components/Seo"
+import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
 
 export default function Home() {    
+    const [movies, setMovies] = useState();
+    useEffect(()=>{        
+        (async()=>{
+            const { results } = await (
+                await fetch(
+                    /* `${BASE_URL}/movie/popular?api_key=${API_KEY}` */
+                    `/api/movies`
+                )
+            ).json();
+            setMovies( results );
+        })();
+    },[]);
     return (
         <div>   
             <Seo title="Home"/>
-            <h1>Hello</h1>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map(movie=>(
+                <div key={movie.id}>
+                    <h4>{movie.original_title}</h4>
+                </div>
+            ))}
         </div>
     );
 }
