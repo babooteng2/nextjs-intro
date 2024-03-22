8ca534357ae0e3b9ab1302ea35dccf99edb65b5d
TomKim <babooteng2@gmail.com>
2024-03-23 00:47:39 +0900

    2024 update #3.4 Parallel Requests

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,26 @@
+import Link from "next/link";
+
 export const metadata = {
     title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-    await new Promise(resolve => setTimeout(resolve, 10000));
-    return fetch(URL).then(response => response.json());
+    //timeout
+    //await new Promise(resolve => setTimeout(resolve, 1000));
+    return fetch(API_URL).then(response => response.json());
 }
 
 export default async function HomePage() {
     const movies = await getMovies();   
     return (
-        <div>            
-            {JSON.stringify(movies)}
-        </div>
+        <ul>
+            {movies.map(movie=>
+                <li key={movie.id}>
+                    <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+                </li>
+            )}
+        </ul>              
     );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,40 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id:String) {
+    console.log(`Fetching movies: ${Date.now()}`)    
+    await new Promise(respose => setTimeout(respose, 5000));
+    const response = await fetch(`${API_URL}/${id}`);
+    return response.json();
+}
+
+async function getVideos(id:String) {
+    console.log(`Fetching videos: ${Date.now()}`)    
+    await new Promise(respose => setTimeout(respose, 5000));
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
+}
+
+export default async function MovieDetail({
     params: {id},
 }: {
     params: {id: string};
 }) {    
-    return <h1>Movie {id}</h1>
+    //await new Promise(respose => setTimeout(respose, 10000));
+    console.log("start fetching");
+    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+    console.log("end fetching");
+    return (
+        <>
+        <h1>{movie.title}</h1>
+        <div>
+            <ul>
+                {videos.map( (video) => 
+                    <li key={video.key}>
+                        {video.name}
+                    </li>
+                )}
+            </ul>
+        </div>
+        </>
+    )
 }
