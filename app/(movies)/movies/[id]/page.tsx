cc163ac5b4ec1516956871a721bac43cfed798ef
TomKim <babooteng2@gmail.com>
2024-03-27 11:01:13 +0900

    2024 update #3.5 Suspense

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,40 +1,18 @@
-import { API_URL } from "../../../(home)/page";
-
-async function getMovie(id:String) {
-    console.log(`Fetching movies: ${Date.now()}`)    
-    await new Promise(respose => setTimeout(respose, 5000));
-    const response = await fetch(`${API_URL}/${id}`);
-    return response.json();
-}
-
-async function getVideos(id:String) {
-    console.log(`Fetching videos: ${Date.now()}`)    
-    await new Promise(respose => setTimeout(respose, 5000));
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    return response.json();
-}
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
 export default async function MovieDetail({
     params: {id},
 }: {
     params: {id: string};
-}) {    
-    //await new Promise(respose => setTimeout(respose, 10000));
-    console.log("start fetching");
-    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-    console.log("end fetching");
-    return (
-        <>
-        <h1>{movie.title}</h1>
-        <div>
-            <ul>
-                {videos.map( (video) => 
-                    <li key={video.key}>
-                        {video.name}
-                    </li>
-                )}
-            </ul>
-        </div>
-        </>
-    )
+}) {
+    return <div>
+        <Suspense fallback={<h1>Loading Movie Info</h1>}>
+            <MovieInfo id={id}/>
+        </Suspense>
+        <Suspense fallback={<h1>Loading Movie Videos</h1>}>
+            <MovieVideos id={id}/>
+        </Suspense>
+    </div>    
 }
