973403ac8153e39e0bae1177cd6e2e046530e05d
TomKim <babooteng2@gmail.com>
2024-04-01 06:42:34 +0900

    2024 update #4.2 Movie Styles

---
+++ components/movie.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Link from "next/link";
+import styles from "../styles/movie.module.css";
+import { useRouter } from "next/navigation";
+
+interface IMovieProps {
+    id: string;
+    title: string;
+    poster_path: string;
+    vote_average: string;
+}
+
+export default function Movie({ id, title, poster_path, vote_average }: IMovieProps) {
+    const router = useRouter();
+    const onClick = () => {
+        router.push(`/movies/${id}`);
+    }
+    return (
+        <div className={styles.movie}>
+            <img 
+                src={poster_path}
+                alt={title} 
+                onClick={onClick}
+            />
+            <Link href={`/movies/${id}`}>
+                <span>{title}</span>
+                <span>{vote_average}</span>
+            </Link>            
+        </div>
+    )
+}
